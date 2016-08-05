4613b50b9544ebd8444d5636ef6866b9b105a80a
taqfu <2buzesn@gmail.com>
2016-08-05 14:49:13 -0400

    Removed background color from notes. Seems clearer now.

--- resources/views/Note/destroy.blade.php
+++ resources/views/Note/destroy.blade.php
@@ -1,13 +1,13 @@
 <form method="POST" action="{{ route('note.destroy', ['id'=>$note->id]) }}" 
-  class='bg-info text-muted {{$type}} inline clearfix'>
+  class='text-muted {{$type}} inline clearfix'><i>
     {{ csrf_field() }}
     {{ method_field('DELETE') }}
     <button type='submit' class='delete btn btn-danger'>
         x
     </button>
     {{ $note->report }}
-    <i class='small'>
+    <span class='small'>
         ( Created - 
         {{ date("m/d/y g:i", strtotime($note->created_at)) }} )
-    </i>
-</form>
+    </span>
+</i></form>
