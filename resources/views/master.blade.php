ae761d3883dae42c4f204994d2f64a12f8148626
taqfu <2buzesn@gmail.com>
2016-08-11 15:10:02 -0400

    Working on landing page

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <title>
-        Root Basis - Quantify Everything
+        Root Basis - Track Time Easily
     </title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
