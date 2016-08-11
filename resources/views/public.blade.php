ba178aa5801187faa8993ea3d469fe8492c19c3a
taqfu <2buzesn@gmail.com>
2016-08-10 20:34:05 -0400

    Added logo

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -1,6 +1,9 @@
 @extends('master')
 
 @section('content')
+<div class='text-center'>
+    <img src='logo-16.png'>
+</div>
     @if (count($errors)>0)
         @foreach($errors as $error)
             {{$error}}
