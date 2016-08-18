a19b6137a6e4f1233a6bddd1b7060251f2025ba5
taqfu <2buzezsn@gmail.com>
2016-08-18 13:16:52 -0700

    Fix: Cannot create a new task category from task type menu.

--- resources/views/TaskCategory/store.blade.php
+++ resources/views/TaskCategory/store.blade.php
@@ -1,7 +1,10 @@
+<?php
+    $task_type_id = isset($task_type->task_type_id) ? $task_type->task_type_id : $task_type->id;
+ ?>
 <form method="POST" action="{{ route('TaskCategory.store') }}"
   id='newTaskCategory{{ $task_type->task_type_id}}' class='newTaskCategory hidden'>
     {{ csrf_field() }}
-    <input type='hidden' name='newTaskTypeID' value='{{ $task_type->task_type_id }}' />
+    <input type='hidden' name='newTaskTypeID' value='{{ $task_type_id }}' />
     <select name='newTaskCategoryTypeID' class='form-control'>
         @foreach ($task_category_types as $task_category_type)
             <option value='{{ $task_category_type->id }}'>
