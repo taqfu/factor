846b7ec339e93ba33eff2d3368b5e06a30c3483b
exnnwc <2buzezsn@gmail.com>
2016-09-17 09:26:18 -0800

    Correcting an issue where task type profile was not properly linked

--- resources/views/TaskType/destroy.blade.php
+++ resources/views/TaskType/destroy.blade.php
@@ -1,7 +1,8 @@
 <?php
     use App\TaskType;
+    $task_type_id = isset($task_type->task_type_id) ? $task_type->task_type_id : $task_type->id; 
 ?>
-<form method="POST" action=" {{ route('TaskType.destroy', ['id'=>$task_type->id])}}"
+<form method="POST" action=" {{ route('TaskType.destroy', ['id'=>$task_type_id])}}"
   onsubmit="return confirm('Are you sure you want to delete \'{{ $task_type->name }}\'');"
   class='inline' >
     {{ csrf_field() }}
@@ -9,7 +10,7 @@
     <button type='submit' class='btn btn-danger'>
         x
     </button>
-    <a href="{{route('TaskType.show', ['id'=>$task_type->id])}}" class='profile-link'>
+    <a href="{{route('TaskType.show', ['id'=>$task_type_id])}}" class='profile-link'>
         {{ $task_type->name }}
     </a>
 </form>
