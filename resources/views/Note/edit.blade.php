5f9cf687b55c57cb7f9c352b1ae726a83b684235
exnnwc <2buzezsn@gmail.com>
2016-09-19 16:50:58 -0800

    Try #3

--- resources/views/Note/edit.blade.php
+++ resources/views/Note/edit.blade.php
@@ -1,11 +1,12 @@
-<form method="POST" action="{{ route('note.update', ['id'=>$note->id]) }}"
-  id='edit-note{{$note->id}}' class='hidden edit-note' />
+<div id='edit-note{{$note->id}}' class='hidden edit-note'>
+    <form method="POST" action="{{ route('note.update', ['id'=>$note->id]) }}"/>
 
-    {{ csrf_field() }}
-    {{ method_field('PUT') }}
-    <textarea name='newReport' class='form-control'>{{ $note->report }}</textarea>
-    <div class='text-right'>
-    <input type='submit' class='btn btn-info' value='Submit' />
-    <input type='button' id='cancel-edit-note{{$note->id}}' class='cancel-edit-note btn btn-info' value='Cancel' />
-    </div>
-</form>
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <textarea name='newReport' class='form-control'>{{ $note->report }}</textarea>
+        <div class='text-right'>
+        <input type='submit' class='btn btn-info' value='Submit' />
+        <input type='button' id='cancel-edit-note{{$note->id}}' class='cancel-edit-note btn btn-info' value='Cancel' />
+        </div>
+    </form>
+</div>
