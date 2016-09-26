93858b8ff62e94da942d810a88e4037bd3aa3dd3
taqfu <2buzezsn@gmail.com>
2016-09-26 13:35:55 -0700

    think i fixed it

--- resources/views/timeSelect.blade.php
+++ resources/views/timeSelect.blade.php
@@ -2,7 +2,6 @@
     use App\User;
 ?>
 <div>
-    {{ var_dump(User::local_now("n j, Y H:i"))}}
     @if($radio)
         <input id='{{ $timestamp_type }}TimestampSelect' type='radio'
           name='{{ $timestamp_type }}When' value='timestamp' />
@@ -13,9 +12,9 @@
         <?php
             $month_val = $month<10 ? "0".$month : $month;
         ?>
-            @if ($month==local_now("n"))
+            @if ($month==User::local_now("n"))
                 <option selected value='{{ $month_val }}' >
-            @elseif ($month!=local_now("n"))
+            @elseif ($month!=User::local_now("n"))
                 <option value='{{ $month_val }}' >
             @endif
         {{ date("F",mktime(0, 0, 0, $month, 10)) }}</option>
@@ -26,9 +25,9 @@
       name="{{ $timestamp_type }}Day">
         @for($day=1 ; $day<32 ; $day++)
         <?php $day_val = $day<10 ? "0".$day : $day;?>
-            @if ($day==local_now("j"))
+            @if ($day==User::local_now("j"))
                 <option selected value='{{ $day_val }}' >
-            @elseif ($day!=local_now("j"))
+            @elseif ($day!=User::local_now("j"))
                 <option value='{{ $day_val }}' >
             @endif
         {{ $day }}</option
@@ -39,9 +38,9 @@
     <select id='{{ $timestamp_type }}Year' class='{{ $timestamp_type }}Timestamp'
       name="{{ $timestamp_type }}Year">
         @for($year=date("Y")-1 ; $year<date("Y")+2 ; $year++)
-            @if ($year==local_now("Y"))
+            @if ($year==User::local_now("Y"))
                 <option selected value='{{ $year }}' >
-            @elseif ($year!=local_now("Y"))
+            @elseif ($year!=User::local_now("Y"))
                 <option value='{{ $year }}' >
             @endif
         {{ $year }}</option>
@@ -56,9 +55,9 @@
     @for ($hour=0; $hour<24; $hour++)
         <?php $hour = $hour<10 ? "0".$hour : $hour; ?>
 
-        @if ($hour==local_now("H"))
+        @if ($hour==User::local_now("H"))
            <option selected value="{{$hour}}">
-        @elseif ($hour!=local_now("H"))
+        @elseif ($hour!=User::local_now("H"))
            <option value="{{$hour}}">
         @endif
         {{ $hour }}</option>
@@ -70,9 +69,9 @@
       name="{{ $timestamp_type }}Minute">
     @for ($minute=0; $minute<60; $minute++)
         <?php $minute = $minute<10 ? "0".$minute : $minute; ?>
-        @if ($minute==local_now("i"))
+        @if ($minute==User::local_now("i"))
            <option selected value="{{$minute}}">
-        @elseif ($minute!=local_now("i"))
+        @elseif ($minute!=User::local_now("i"))
            <option value="{{$minute}}">
         @endif
         {{ $minute }}</option>
